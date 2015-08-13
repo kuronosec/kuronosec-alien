3658322cd0f90299e3db56b918f3a8fe7b98e1f7
kurono <andres.gomez@iri.uni-frankfurt.de>
2015-08-13 16:14:15 +0200

    Feature #12: fix node installation.

--- manifests/node/config.pp
+++ manifests/node/config.pp
@@ -1,14 +1,14 @@
 class alien::node::config inherits alien {
 
-    file_line { 'aliprod_alien_path':
+    file_line { 'alien_path':
         path    => "$user_home/.bashrc",
         line    => 'export PATH="$HOME/alien/bin:$PATH"',
-        require => User['aliprod_user'],
+        require => User['alien_user'],
     }
 
-    file_line { 'aliprod_alien_lang':
+    file_line { 'alien_lang':
         path    => "$user_home/.bashrc",
         line    => 'export LANG=C',
-        require => User['aliprod_user'],
+        require => User['alien_user'],
     }
 }
--- manifests/node/install.pp
+++ manifests/node/install.pp
@@ -4,9 +4,9 @@ class alien::node::install inherits alien {
     exec { 'install_alien':
         creates => "$user_home/alien",
         path    => '/usr/bin:/bin',
-        command => "curl http://alien.cern.ch/alien-installer -o alien-installer && chmod +x alien-installer && su $user_name -c './alien-installer -version $alien_version -type worker$
+        command => "curl http://alien.cern.ch/alien-installer -o alien-installer && chmod +x alien-installer && su $user_name -c './alien-installer -version $alien_version -type workernode -batch -notorrent -install-dir $user_home/alien'",
         cwd     => '/tmp',
         timeout     => 1800,
-        require => User['aliprod_user'],
+        require => User['alien_user'],
     }
 }
