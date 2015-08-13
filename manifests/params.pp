07de10b1b28a9054d3d3df0a9d51e34a5547447b
kurono <andres.gomez@iri.uni-frankfurt.de>
2015-08-13 15:59:22 +0200

    Feature #12: fix the alien version for installation.

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,6 +9,6 @@ class alien::params {
     $service_ensure = 'running'
     $service_enable = 'true'
     $service_name   = 'alien_middleware'
-    $alien_version  = 'v2-19.300'
+    $alien_version  = 'v2-19'
 }
 
